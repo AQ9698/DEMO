508bf3d34ef09733bc7d21fff67ce9aa712915cf
AQ9698 <1047722409@qq.com>
2019-01-23 19:05:25 +0800

    first commit

---
+++ express/routes/ajx.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+var mg = require('./mg');
+
+/* GET home page. */
+router.get('/', function(req, res, next){
+  
+  console.log('ajx',req.query)
+  // req.session.username=req.query.username;
+  // console.log(req.session);
+  //读库
+  mg({
+    // url:'mongodb://127.0.0.1:27017',
+    dbName:'1228',
+    collectionName:'user'
+  },(collection,client)=>{
+    collection.find({
+      username:req.query.username,
+      password:req.query.password
+    }).toArray((err,data)=>{
+      // if(data.length>0){
+      //   res.send(data[0])
+      // }else{
+      //   res.send({error:1,msg:'用户名或者密码有误'})
+      // }
+      console.log(data)
+      if(data.length>0){
+        if(req.query.password==data[0].password&&req.query.username==data[0].username){
+          res.send("2") //登陆成功
+        // res.send({data:data})
+        }else{ 
+         res.send("0") //用户名或者密码错误
+        } 
+      }else{
+        res.send("1") //用户名密码为空
+      }
+      client.close()
+    })
+  })
+  
+
+});
+
+router.post('/', function(req, res, next) {
+  
+  console.log('ajx',req.body)
+
+  //读库
+  
+
+});
+
+module.exports = router;
